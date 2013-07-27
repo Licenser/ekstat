5b4fdbd24f0afdc4f1f518ee43afb251448f287e
Andrew Bennett <andrew@pagodabox.com>
2013-07-26 18:56:03 -0700

    Adding cpu_stat and misc unix sysinfo, var, and vminfo.

--- c_src/ekstat.c
+++ c_src/ekstat.c
@@ -3,6 +3,8 @@
 #include <kstat.h>
 #include <stdio.h>
 #include <errno.h>
+#include <sys/sysinfo.h>
+#include <sys/var.h>
 
 ErlNifResourceType* kstat_handle;
 
@@ -116,6 +118,164 @@ ERL_NIF_TERM data_io(ErlNifEnv* env, kstat_t *ksp) {
   return enif_make_list_from_array(env, data, 12);
 };
 
+ERL_NIF_TERM data_raw_cpu_stat(ErlNifEnv* env, kstat_t *ksp) {
+  cpu_sysinfo_t *cpu_sysinfop;
+  cpu_sysinfop = (cpu_sysinfo_t *)(ksp->ks_data);
+#ifdef STATISTICS
+  ERL_NIF_TERM data[59] =
+#else
+  ERL_NIF_TERM data[53] =
+#endif
+  {
+    named_tuple(env, "idle", cpu_sysinfop->cpu[CPU_IDLE]),
+    named_tuple(env, "user", cpu_sysinfop->cpu[CPU_USER]),
+    named_tuple(env, "kernel", cpu_sysinfop->cpu[CPU_KERNEL]),
+    named_tuple(env, "wait", cpu_sysinfop->cpu[CPU_WAIT]),
+    named_tuple(env, "wait_io", cpu_sysinfop->wait[W_IO]),
+    named_tuple(env, "wait_swap", cpu_sysinfop->wait[W_SWAP]),
+    named_tuple(env, "wait_pio", cpu_sysinfop->wait[W_PIO]),
+
+    named_tuple(env, "bread", cpu_sysinfop->bread),
+    named_tuple(env, "bwrite", cpu_sysinfop->bwrite),
+    named_tuple(env, "lread", cpu_sysinfop->lread),
+    named_tuple(env, "lwrite", cpu_sysinfop->lwrite),
+    named_tuple(env, "phread", cpu_sysinfop->phread),
+    named_tuple(env, "phwrite", cpu_sysinfop->phwrite),
+    named_tuple(env, "pswitch", cpu_sysinfop->pswitch),
+    named_tuple(env, "trap", cpu_sysinfop->trap),
+    named_tuple(env, "intr", cpu_sysinfop->intr),
+    named_tuple(env, "syscall", cpu_sysinfop->syscall),
+    named_tuple(env, "sysread", cpu_sysinfop->sysread),
+    named_tuple(env, "syswrite", cpu_sysinfop->syswrite),
+    named_tuple(env, "sysfork", cpu_sysinfop->sysfork),
+    named_tuple(env, "sysvfork", cpu_sysinfop->sysvfork),
+    named_tuple(env, "sysexec", cpu_sysinfop->sysexec),
+    named_tuple(env, "readch", cpu_sysinfop->readch),
+    named_tuple(env, "writech", cpu_sysinfop->writech),
+    named_tuple(env, "rcvint", cpu_sysinfop->rcvint),
+    named_tuple(env, "xmtint", cpu_sysinfop->xmtint),
+    named_tuple(env, "mdmint", cpu_sysinfop->mdmint),
+    named_tuple(env, "rawch", cpu_sysinfop->rawch),
+    named_tuple(env, "canch", cpu_sysinfop->canch),
+    named_tuple(env, "outch", cpu_sysinfop->outch),
+    named_tuple(env, "msg", cpu_sysinfop->msg),
+    named_tuple(env, "sema", cpu_sysinfop->sema),
+    named_tuple(env, "namei", cpu_sysinfop->namei),
+    named_tuple(env, "ufsiget", cpu_sysinfop->ufsiget),
+    named_tuple(env, "ufsdirblk", cpu_sysinfop->ufsdirblk),
+    named_tuple(env, "ufsipage", cpu_sysinfop->ufsipage),
+    named_tuple(env, "ufsinopage", cpu_sysinfop->ufsinopage),
+    named_tuple(env, "inodeovf", cpu_sysinfop->inodeovf),
+    named_tuple(env, "fileovf", cpu_sysinfop->fileovf),
+    named_tuple(env, "procovf", cpu_sysinfop->procovf),
+    named_tuple(env, "intrthread", cpu_sysinfop->intrthread),
+    named_tuple(env, "intrblk", cpu_sysinfop->intrblk),
+    named_tuple(env, "idlethread", cpu_sysinfop->idlethread),
+    named_tuple(env, "inv_swtch", cpu_sysinfop->inv_swtch),
+    named_tuple(env, "nthreads", cpu_sysinfop->nthreads),
+    named_tuple(env, "cpumigrate", cpu_sysinfop->cpumigrate),
+    named_tuple(env, "xcalls", cpu_sysinfop->xcalls),
+    named_tuple(env, "mutex_adenters", cpu_sysinfop->mutex_adenters),
+    named_tuple(env, "rw_rdfails", cpu_sysinfop->rw_rdfails),
+    named_tuple(env, "rw_wrfails", cpu_sysinfop->rw_wrfails),
+    named_tuple(env, "modload", cpu_sysinfop->modload),
+    named_tuple(env, "modunload", cpu_sysinfop->modunload),
+    named_tuple(env, "bawrite", cpu_sysinfop->bawrite)
+#ifdef STATISTICS
+    , named_tuple(env, "rw_enters", cpu_sysinfop->rw_enters),
+    named_tuple(env, "win_uo_cnt", cpu_sysinfop->win_uo_cnt),
+    named_tuple(env, "win_uu_cnt", cpu_sysinfop->win_uu_cnt),
+    named_tuple(env, "win_so_cnt", cpu_sysinfop->win_so_cnt),
+    named_tuple(env, "win_su_cnt", cpu_sysinfop->win_su_cnt),
+    named_tuple(env, "win_suo_cnt", cpu_sysinfop->win_suo_cnt)};
+  return enif_make_list_from_array(env, data, 59);
+#else
+  };
+  return enif_make_list_from_array(env, data, 53);
+#endif
+};
+
+ERL_NIF_TERM data_raw_sysinfo(ErlNifEnv* env, kstat_t *ksp) {
+  sysinfo_t* sysinfop;
+  sysinfop = (sysinfo_t *)(ksp->ks_data);
+  ERL_NIF_TERM data[6] = {named_tuple(env, "updates", sysinfop->updates),
+                          named_tuple(env, "runque", sysinfop->runque),
+                          named_tuple(env, "runocc", sysinfop->runocc),
+                          named_tuple(env, "swpque",sysinfop->swpque),
+                          named_tuple(env, "swpocc", sysinfop->swpocc),
+                          named_tuple(env, "waiting", sysinfop->waiting)};
+  return enif_make_list_from_array(env, data, 6);
+};
+
+ERL_NIF_TERM data_raw_var(ErlNifEnv* env, kstat_t *ksp) {
+  struct var *varp;
+  varp = (struct var *)(ksp->ks_data);
+  ERL_NIF_TERM data[15] = {
+    named_tuple(env, "v_buf", varp->v_buf),
+    named_tuple(env, "v_call", varp->v_call),
+    named_tuple(env, "v_proc", varp->v_proc),
+    named_tuple(env, "v_maxupttl", varp->v_maxupttl),
+    named_tuple(env, "v_nglobpris", varp->v_nglobpris),
+    named_tuple(env, "v_maxsyspri", varp->v_maxsyspri),
+    named_tuple(env, "v_clist", varp->v_clist),
+    named_tuple(env, "v_maxup", varp->v_maxup),
+    named_tuple(env, "v_hbuf", varp->v_hbuf),
+    named_tuple(env, "v_hmask", varp->v_hmask),
+    named_tuple(env, "v_pbuf", varp->v_pbuf),
+    named_tuple(env, "v_sptmap", varp->v_sptmap),
+    named_tuple(env, "v_maxpmem", varp->v_maxpmem),
+    named_tuple(env, "v_autoup", varp->v_autoup),
+    named_tuple(env, "v_bufhwm", varp->v_bufhwm)
+  };
+  return enif_make_list_from_array(env, data, 15);
+};
+
+ERL_NIF_TERM data_raw_vminfo(ErlNifEnv* env, kstat_t *ksp) {
+  vminfo_t *vminfop;
+  vminfop = (vminfo_t *)(ksp->ks_data);
+  ERL_NIF_TERM data[6] = {named_tuple(env, "freemem", vminfop->freemem),
+                          named_tuple(env, "swap_resv", vminfop->swap_resv),
+                          named_tuple(env, "swap_alloc", vminfop->swap_alloc),
+                          named_tuple(env, "swap_avail",vminfop->swap_avail),
+                          named_tuple(env, "swap_free", vminfop->swap_free),
+                          named_tuple(env, "updates", vminfop->updates)};
+  return enif_make_list_from_array(env, data, 6);
+};
+
+ERL_NIF_TERM data_raw(ErlNifEnv* env, kstat_t *ksp) {
+  ERL_NIF_TERM data;
+
+  if (!strcmp(ksp->ks_module, "cpu_stat")) {
+    data = data_raw_cpu_stat(env, ksp);
+  } else if (!strcmp(ksp->ks_module, "unix")) {
+    if (!strcmp(ksp->ks_name, "sysinfo")) {
+      data = data_raw_sysinfo(env, ksp);
+    } else if (!strcmp(ksp->ks_name, "var")) {
+      data = data_raw_var(env, ksp);
+    } else if (!strcmp(ksp->ks_name, "vminfo")) {
+      data = data_raw_vminfo(env, ksp);
+    } else {
+      char error[256];
+      snprintf(error, sizeof error, "unknown raw unix ksp->ks_name: %s", ksp->ks_name);
+      data = enif_make_tuple2(env,
+                              enif_make_atom(env, "error"),
+                              enif_make_string(env,
+                                               error,
+                                               ERL_NIF_LATIN1));
+    }
+  } else {
+    char error[256];
+    snprintf(error, sizeof error, "unknown raw ksp->ks_module: %s", ksp->ks_module);
+    data = enif_make_tuple2(env,
+                            enif_make_atom(env, "error"),
+                            enif_make_string(env,
+                                             error,
+                                             ERL_NIF_LATIN1));
+  }
+
+  return data;
+};
+
 ERL_NIF_TERM read_entry(ErlNifEnv* env, kstat_ctl_t *ksr_ctl, kstat_t *ksp) {
   ERL_NIF_TERM data;
   if (kstat_read(ksr_ctl, ksp, NULL) == -1) {
@@ -134,16 +294,36 @@ ERL_NIF_TERM read_entry(ErlNifEnv* env, kstat_ctl_t *ksr_ctl, kstat_t *ksp) {
                                              ERL_NIF_LATIN1));
     return data;
   }
-  if (ksp->ks_type == KSTAT_TYPE_NAMED) {
-    data = data_named(env, ksp);
-  } else if (ksp->ks_type == KSTAT_TYPE_IO) {
-    data = data_io(env, ksp);
-  } else {
-    data = enif_make_tuple2(env,
-                            enif_make_atom(env, "error"),
-                            enif_make_string(env,
-                                             "unknown ksp",
-                                             ERL_NIF_LATIN1));
+  switch(ksp->ks_type) {
+    case KSTAT_TYPE_NAMED:
+      data = data_named(env, ksp);
+      break;
+    case KSTAT_TYPE_IO:
+      data = data_io(env, ksp);
+      break;
+    case KSTAT_TYPE_TIMER:
+      data = enif_make_tuple2(env,
+                              enif_make_atom(env, "error"),
+                              enif_make_string(env,
+                                               "unknown timer",
+                                               ERL_NIF_LATIN1));
+      break;
+    case KSTAT_TYPE_INTR:
+      data = enif_make_tuple2(env,
+                              enif_make_atom(env, "error"),
+                              enif_make_string(env,
+                                               "unknown intr",
+                                               ERL_NIF_LATIN1));
+      break;
+    case KSTAT_TYPE_RAW:
+      data = data_raw(env, ksp);
+      break;
+    default:
+      data = enif_make_tuple2(env,
+                              enif_make_atom(env, "error"),
+                              enif_make_string(env,
+                                               "unknown ksp",
+                                               ERL_NIF_LATIN1));
   }
   return data;
 }
