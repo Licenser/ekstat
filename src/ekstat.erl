91fd55c19c80f275a7f1735dd8941635bca80350
Andrew Bennett <andrew@pagodabox.com>
2013-07-30 13:03:02 -0600

    Updated README.md and adding CHANGES.md.

--- src/ekstat.erl
+++ src/ekstat.erl
@@ -1,15 +1,18 @@
 -module(ekstat).
 
 %% API
--export([open/0, update/1, read/1, read/2, read/3, read/4, read/5, read/6, clear/1]).
+-export([open/0, update/1, clear/1, read/1, read/2, read/3, read/4, read/5, read/6]).
 
 -on_load(init/0).
 
 %% Types
 -type kstat_handle() :: binary().
+-type kstat_pattern() :: '_' | integer() | string().
 -type kstat_error() :: {error, {kstat, string()}}.
 -type kstat_statistic() :: {Statistic::string(), Value::integer()} | {Statistic::string(), Value::string()}.
 -type kstat_row() :: {Class::string(), Module::string(), Instance::integer(), Name::string(), [kstat_statistic()]}.
+
+-export_type([kstat_handle/0, kstat_pattern/0]).
 
 %%%===================================================================
 %%% API functions
@@ -19,36 +22,36 @@
 open() ->
     erlang:nif_error(undefined).
 
--spec update(Handle::binary()) -> [kstat_row()] | kstat_error().
+-spec update(Handle::binary()) -> {ok, NumInstances::integer()} | kstat_error().
 update(_Handle) ->
+    erlang:nif_error(undefined).
+
+-spec clear(Handle::binary()) -> {ok, NumInstances::integer()} | kstat_error().
+clear(_Handle) ->
     erlang:nif_error(undefined).
 
 -spec read(Handle::binary()) -> [kstat_row()] | kstat_error().
 read(_Handle) ->
     erlang:nif_error(undefined).
 
--spec read(Handle::binary(), Class::string()) -> [kstat_row()] | kstat_error().
+-spec read(Handle::binary(), Class::kstat_pattern()) -> [kstat_row()] | kstat_error().
 read(_Handle, _Class) ->
     erlang:nif_error(undefined).
 
--spec read(Handle::binary(), Class::string(), Module::string()) -> [kstat_row()] | kstat_error().
+-spec read(Handle::binary(), Class::kstat_pattern(), Module::kstat_pattern()) -> [kstat_row()] | kstat_error().
 read(_Handle, _Class, _Module) ->
     erlang:nif_error(undefined).
 
--spec read(Handle::binary(), Class::string(), Module::string(), Instance::integer()) -> [kstat_row()] | kstat_error().
+-spec read(Handle::binary(), Class::kstat_pattern(), Module::kstat_pattern(), Instance::kstat_pattern()) -> [kstat_row()] | kstat_error().
 read(_Handle, _Class, _Module, _Instance) ->
     erlang:nif_error(undefined).
 
--spec read(Handle::binary(), Class::string(), Module::string(), Instance::integer(), Name::string()) -> [kstat_row()] | kstat_error().
+-spec read(Handle::binary(), Class::kstat_pattern(), Module::kstat_pattern(), Instance::kstat_pattern(), Name::kstat_pattern()) -> [kstat_row()] | kstat_error().
 read(_Handle, _Class, _Module, _Instance, _Name) ->
     erlang:nif_error(undefined).
 
--spec read(Handle::binary(), Class::string(), Module::string(), Instance::integer(), Name::string(), Statistic::string()) -> [kstat_row()] | kstat_error().
+-spec read(Handle::binary(), Class::kstat_pattern(), Module::kstat_pattern(), Instance::kstat_pattern(), Name::kstat_pattern(), Statistic::kstat_pattern()) -> [kstat_row()] | kstat_error().
 read(_Handle, _Class, _Module, _Instance, _Name, _Statistic) ->
-    erlang:nif_error(undefined).
-
--spec clear(Handle::binary()) -> [kstat_row()] | kstat_error().
-clear(_Handle) ->
     erlang:nif_error(undefined).
 
 %%%-------------------------------------------------------------------
