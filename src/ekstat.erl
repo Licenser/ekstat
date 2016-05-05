619377078c3b5df1f327fa0916e463bf67b18fc7
Andrew Bennett <potatosaladx@gmail.com>
2016-05-05 16:44:51 -0600

    Fix formatting errors so elvis doesn't yell at us.

--- src/ekstat.erl
+++ src/ekstat.erl
@@ -1,7 +1,16 @@
 -module(ekstat).
 
 %% API
--export([open/0, close/1, clear/1, update/1, read/1, read/2, read/3, read/4, read/5, read/6]).
+-export([open/0]).
+-export([close/1]).
+-export([clear/1]).
+-export([update/1]).
+-export([read/1]).
+-export([read/2]).
+-export([read/3]).
+-export([read/4]).
+-export([read/5]).
+-export([read/6]).
 
 -on_load(init/0).
 
@@ -12,11 +21,18 @@
 -endif.
 
 %% Types
--type kstat_handle() :: binary().
--type kstat_pattern() :: '_' | integer() | string().
--type kstat_error() :: {error, atom()} | {error, {atom(), string()}}.
--type kstat_statistic() :: {Statistic::string(), Value::integer()} | {Statistic::string(), Value::string()}.
--type kstat_row() :: {Class::string(), Module::string(), Instance::integer(), Name::string(), [kstat_statistic()]}.
+-type kstat_handle() ::
+    binary().
+-type kstat_pattern() ::
+    '_' | integer() | string().
+-type kstat_error() ::
+    {error, atom()} | {error, {atom(), string()}}.
+-type kstat_statistic() ::
+    {Statistic::string(), Value::integer()} |
+    {Statistic::string(), Value::string()}.
+-type kstat_row() ::
+    {Class::string(), Module::string(), Instance::integer(),
+        Name::string(), [kstat_statistic()]}.
 
 -export_type([kstat_handle/0, kstat_pattern/0]).
 
@@ -24,45 +40,98 @@
 %%% API functions
 %%%===================================================================
 
--spec open() -> {ok, Handle::kstat_handle()} | kstat_error().
+-spec open() ->
+    {ok, Handle::kstat_handle()} | kstat_error().
 open() ->
-    ?ASYNC_NIF_CALL(fun open_nif/1, []).
+    ?ASYNC_NIF_CALL(fun open_nif/1,
+        []).
 
--spec close(Handle::binary()) -> ok | kstat_error().
+-spec close(Handle) ->
+    ok | kstat_error()
+    when
+        Handle :: binary().
 close(Handle) ->
-    ?ASYNC_NIF_CALL(fun close_nif/2, [Handle]).
+    ?ASYNC_NIF_CALL(fun close_nif/2,
+        [Handle]).
 
--spec clear(Handle::binary()) -> {ok, NumInstances::integer()} | kstat_error().
+-spec clear(Handle) ->
+    {ok, NumInstances::integer()} | kstat_error()
+    when
+        Handle :: binary().
 clear(Handle) ->
-    ?ASYNC_NIF_CALL(fun clear_nif/2, [Handle]).
+    ?ASYNC_NIF_CALL(fun clear_nif/2,
+        [Handle]).
 
--spec update(Handle::binary()) -> {ok, NumInstances::integer()} | kstat_error().
+-spec update(Handle) ->
+    {ok, NumInstances::integer()} | kstat_error()
+    when
+        Handle :: binary().
 update(Handle) ->
-    ?ASYNC_NIF_CALL(fun update_nif/2, [Handle]).
+    ?ASYNC_NIF_CALL(fun update_nif/2,
+        [Handle]).
 
--spec read(Handle::binary()) -> [kstat_row()] | kstat_error().
+-spec read(Handle) ->
+    [kstat_row()] | kstat_error()
+    when
+        Handle :: binary().
 read(Handle) ->
-    ?ASYNC_NIF_CALL(fun read_nif/2, [Handle]).
+    ?ASYNC_NIF_CALL(fun read_nif/2,
+        [Handle]).
 
--spec read(Handle::binary(), Class::kstat_pattern()) -> [kstat_row()] | kstat_error().
+-spec read(Handle, Class) ->
+    [kstat_row()] | kstat_error()
+    when
+        Handle :: binary(),
+        Class :: kstat_pattern().
 read(Handle, Class) ->
-    ?ASYNC_NIF_CALL(fun read_nif/3, [Handle, Class]).
+    ?ASYNC_NIF_CALL(fun read_nif/3,
+        [Handle, Class]).
 
--spec read(Handle::binary(), Class::kstat_pattern(), Module::kstat_pattern()) -> [kstat_row()] | kstat_error().
+-spec read(Handle, Class, Module) ->
+    [kstat_row()] | kstat_error()
+    when
+        Handle :: binary(),
+        Class :: kstat_pattern(),
+        Module :: kstat_pattern().
 read(Handle, Class, Module) ->
-    ?ASYNC_NIF_CALL(fun read_nif/4, [Handle, Class, Module]).
+    ?ASYNC_NIF_CALL(fun read_nif/4,
+        [Handle, Class, Module]).
 
--spec read(Handle::binary(), Class::kstat_pattern(), Module::kstat_pattern(), Instance::kstat_pattern()) -> [kstat_row()] | kstat_error().
+-spec read(Handle, Class, Module, Instance) ->
+    [kstat_row()] | kstat_error()
+    when
+        Handle :: binary(),
+        Class :: kstat_pattern(),
+        Module :: kstat_pattern(),
+        Instance :: kstat_pattern().
 read(Handle, Class, Module, Instance) ->
-    ?ASYNC_NIF_CALL(fun read_nif/5, [Handle, Class, Module, Instance]).
+    ?ASYNC_NIF_CALL(fun read_nif/5,
+        [Handle, Class, Module, Instance]).
 
--spec read(Handle::binary(), Class::kstat_pattern(), Module::kstat_pattern(), Instance::kstat_pattern(), Name::kstat_pattern()) -> [kstat_row()] | kstat_error().
+-spec read(Handle, Class, Module, Instance, Name) ->
+    [kstat_row()] | kstat_error()
+    when
+        Handle :: binary(),
+        Class :: kstat_pattern(),
+        Module :: kstat_pattern(),
+        Instance :: kstat_pattern(),
+        Name :: kstat_pattern().
 read(Handle, Class, Module, Instance, Name) ->
-    ?ASYNC_NIF_CALL(fun read_nif/6, [Handle, Class, Module, Instance, Name]).
+    ?ASYNC_NIF_CALL(fun read_nif/6,
+        [Handle, Class, Module, Instance, Name]).
 
--spec read(Handle::binary(), Class::kstat_pattern(), Module::kstat_pattern(), Instance::kstat_pattern(), Name::kstat_pattern(), Statistic::kstat_pattern()) -> [kstat_row()] | kstat_error().
+-spec read(Handle, Class, Module, Instance, Name, Statistic) ->
+    [kstat_row()] | kstat_error()
+    when
+        Handle :: binary(),
+        Class :: kstat_pattern(),
+        Module :: kstat_pattern(),
+        Instance :: kstat_pattern(),
+        Name :: kstat_pattern(),
+        Statistic :: kstat_pattern().
 read(Handle, Class, Module, Instance, Name, Statistic) ->
-    ?ASYNC_NIF_CALL(fun read_nif/7, [Handle, Class, Module, Instance, Name, Statistic]).
+    ?ASYNC_NIF_CALL(fun read_nif/7,
+        [Handle, Class, Module, Instance, Name, Statistic]).
 
 %%%-------------------------------------------------------------------
 %%% NIF functions
@@ -103,7 +172,10 @@ read_nif(_AsyncRef, _Handle, _Class, _Module, _Instance, _Name, _Statistic) ->
 %%%-------------------------------------------------------------------
 
 %% @private
--spec init() -> ok | {error, {Reason::load_failed | bad_lib | load | reload | upgrade | old_code, Text::string()}}.
+-spec init() ->
+    ok | {error, {Reason, Text::string()}}
+    when
+        Reason :: load_failed | bad_lib | load | reload | upgrade | old_code.
 init() ->
     PrivDir = case code:priv_dir(?MODULE) of
                   {error, _} ->
